2232a8d79736350e0e3e49f5b9b7601c0a6db58b
Derya Cortuk <53267226+deryacortuk@users.noreply.github.com>
2023-03-13 20:52:51 +0300

    Update settings.py

--- api/settings.py
+++ api/settings.py
@@ -1,11 +1,11 @@
 payload = {
-    "username": "****",
-    "password": "***"
+    "username": "365",
+    "password": "1"
 }
 headers = {
-    "Authorization": "Basic *******",
+    "Authorization": "Basic QVBJX0V4cGxvcmVyOjEyMzQ1NmlzQUxhbWVQYXNz",
     "Content-Type": "application/json"
 }
         
     
-base_url = "https://api.********"  
+base_url = "https://api.baubuddy.de" 
